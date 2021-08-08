44963a8e9173df025d85e0df67569de0700d3681
doodl <doodles230905@gmail.com>
2021-08-08 11:19:27 +0300

    add /api/map route

--- ctf-server.py
+++ ctf-server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from config import *
 
 app = Flask(__name__)
@@ -6,5 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Here I am!"
+
+@app.route('/api/map')
+def get_map():
+
+    M = [
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+
+    return jsonify(M)
+
 
 app.run(HOST, PORT, debug=DEBUG)
