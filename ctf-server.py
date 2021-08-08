3a49fefb53945dfca531ea1cc51c3b63a1cd791d
doodl <doodles230905@gmail.com>
2021-08-08 12:37:55 +0300

    registration

--- ctf-server.py
+++ ctf-server.py
@@ -3,13 +3,14 @@ from config import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Here I am!"
 
+
 @app.route('/api/map')
 def get_map():
-
     M = [
         [0, 0, 0, 0],
         [1, 0, 0, 0],
