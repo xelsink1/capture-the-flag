32c3b265bca7ee6bb8a94dbb446b8d6bf2f7cf38
xelsink1 <88479566+xelsink1@users.noreply.github.com>
2021-08-09 14:25:10 +0300

    Update gener_labirint.py

--- gener_labirint.py
+++ gener_labirint.py
@@ -1,108 +1,215 @@
-
-import pygame
-from random import choice
-
-RES = WIDTH, HEIGHT = 500, 500
-TILE = 32
-cols, rows = WIDTH // TILE, HEIGHT // TILE
-
-pygame.init()
-sc = pygame.display.set_mode(RES)
-clock = pygame.time.Clock()
+import random
+from colorama import init
+from colorama import Fore, Back, Style
 
 
-class Cell:
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-        self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
-        self.visited = False
+def printMaze(maze):
+    for i in range(0, height):
+        for j in range(0, width):
+            if (maze[i][j] == '2'):
+                print(str(maze[i][j]), end=" ")
+            elif (maze[i][j] == '#'):
+                print(str(maze[i][j]), end=" ")
+            else:
+                print(str(maze[i][j]), end=" ")
 
-    def draw_current_cell(self):
-        x, y = self.x * TILE, self.y * TILE
-        pygame.draw.rect(sc, pygame.Color('saddlebrown'), (x + 2, y + 2, TILE - 2, TILE - 2))
-
-    def draw(self):
-        x, y = self.x * TILE, self.y * TILE
-        if self.visited:
-            pygame.draw.rect(sc, pygame.Color('black'), (x, y, TILE, TILE))
-
-        if self.walls['top']:
-            pygame.draw.line(sc, pygame.Color('darkorange'), (x, y), (x + TILE, y), 3)
-        if self.walls['right']:
-            pygame.draw.line(sc, pygame.Color('darkorange'), (x + TILE, y), (x + TILE, y + TILE), 3)
-        if self.walls['bottom']:
-            pygame.draw.line(sc, pygame.Color('darkorange'), (x + TILE, y + TILE), (x, y + TILE), 3)
-        if self.walls['left']:
-            pygame.draw.line(sc, pygame.Color('darkorange'), (x, y + TILE), (x, y), 3)
-
-    def check_cell(self, x, y):
-        find_index = lambda x, y: x + y * cols
-        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
-            return False
-        return grid_cells[find_index(x, y)]
-
-    def check_neighbors(self):
-        neighbors = []
-        top = self.check_cell(self.x, self.y - 1)
-        right = self.check_cell(self.x + 1, self.y)
-        bottom = self.check_cell(self.x, self.y + 1)
-        left = self.check_cell(self.x - 1, self.y)
-        if top and not top.visited:
-            neighbors.append(top)
-        if right and not right.visited:
-            neighbors.append(right)
-        if bottom and not bottom.visited:
-            neighbors.append(bottom)
-        if left and not left.visited:
-            neighbors.append(left)
-        return choice(neighbors) if neighbors else False
+        print('\n')
 
 
-def remove_walls(current, next):
-    dx = current.x - next.x
-    if dx == 1:
-        current.walls['left'] = False
-        next.walls['right'] = False
-    elif dx == -1:
-        current.walls['right'] = False
-        next.walls['left'] = False
-    dy = current.y - next.y
-    if dy == 1:
-        current.walls['top'] = False
-        next.walls['bottom'] = False
-    elif dy == -1:
-        current.walls['bottom'] = False
-        next.walls['top'] = False
+def surroundingCells(rand_wall):
+    s_cells = 0
+    if (maze[rand_wall[0] - 1][rand_wall[1]] == 'c'):
+        s_cells += 1
+    if (maze[rand_wall[0] + 1][rand_wall[1]] == 'c'):
+        s_cells += 1
+    if (maze[rand_wall[0]][rand_wall[1] - 1] == 'c'):
+        s_cells += 1
+    if (maze[rand_wall[0]][rand_wall[1] + 1] == 'c'):
+        s_cells += 1
+
+    return s_cells
 
 
-grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
-current_cell = grid_cells[0]
-stack = []
-colors, color = [], 40
+wall = '#'
+cell = '.'
+unvisited = '2'
+height = 32
+width = 32
+maze = []
 
-while True:
-    sc.fill(pygame.Color('darkslategray'))
+init()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+for i in range(0, height):
+    line = []
+    for j in range(0, width):
+        line.append(unvisited)
+    maze.append(line)
 
-    [cell.draw() for cell in grid_cells]
-    current_cell.visited = True
-    current_cell.draw_current_cell()
-    [pygame.draw.rect(sc, colors[i], (cell.x * TILE + 2, cell.y * TILE + 2,
-                                      TILE - 4, TILE - 4)) for i, cell in enumerate(stack)]
+starting_height = int(random.random() * height)
+starting_width = int(random.random() * width)
+if (starting_height == 0):
+    starting_height += 1
+if (starting_height == height - 1):
+    starting_height -= 1
+if (starting_width == 0):
+    starting_width += 1
+if (starting_width == width - 1):
+    starting_width -= 1
 
-    next_cell = current_cell.check_neighbors()
-    if next_cell:
-        next_cell.visited = True
-        stack.append(current_cell)
-        colors.append((min(color, 255), 10, 100))
-        color += 1
-        remove_walls(current_cell, next_cell)
-        current_cell = next_cell
-    elif stack:
-        current_cell = stack.pop()
+maze[starting_height][starting_width] = cell
+walls = []
+walls.append([starting_height - 1, starting_width])
+walls.append([starting_height, starting_width - 1])
+walls.append([starting_height, starting_width + 1])
+walls.append([starting_height + 1, starting_width])
 
-    pygame.display.flip()
-    clock.tick(30)
+maze[starting_height - 1][starting_width] = '#'
+maze[starting_height][starting_width - 1] = '#'
+maze[starting_height][starting_width + 1] = '#'
+maze[starting_height + 1][starting_width] = '#'
+
+while (walls):
+
+    rand_wall = walls[int(random.random() * len(walls)) - 1]
+
+    if (rand_wall[1] != 0):
+        if (maze[rand_wall[0]][rand_wall[1] - 1] == '2' and maze[rand_wall[0]][rand_wall[1] + 1] == '.'):
+
+            s_cells = surroundingCells(rand_wall)
+
+            if (s_cells < 2):
+                maze[rand_wall[0]][rand_wall[1]] = '.'
+
+                if (rand_wall[0] != 0):
+                    if (maze[rand_wall[0] - 1][rand_wall[1]] != '.'):
+                        maze[rand_wall[0] - 1][rand_wall[1]] = '#'
+                    if ([rand_wall[0] - 1, rand_wall[1]] not in walls):
+                        walls.append([rand_wall[0] - 1, rand_wall[1]])
+
+                if (rand_wall[0] != height - 1):
+                    if (maze[rand_wall[0] + 1][rand_wall[1]] != '.'):
+                        maze[rand_wall[0] + 1][rand_wall[1]] = '#'
+                    if ([rand_wall[0] + 1, rand_wall[1]] not in walls):
+                        walls.append([rand_wall[0] + 1, rand_wall[1]])
+
+                if (rand_wall[1] != 0):
+                    if (maze[rand_wall[0]][rand_wall[1] - 1] != '.'):
+                        maze[rand_wall[0]][rand_wall[1] - 1] = '#'
+                    if ([rand_wall[0], rand_wall[1] - 1] not in walls):
+                        walls.append([rand_wall[0], rand_wall[1] - 1])
+
+            for wall in walls:
+                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                    walls.remove(wall)
+
+            continue
+
+    if (rand_wall[0] != 0):
+        if (maze[rand_wall[0] - 1][rand_wall[1]] == '2' and maze[rand_wall[0] + 1][rand_wall[1]] == '.'):
+
+            s_cells = surroundingCells(rand_wall)
+            if (s_cells < 2):
+                maze[rand_wall[0]][rand_wall[1]] = '.'
+
+                if (rand_wall[0] != 0):
+                    if (maze[rand_wall[0] - 1][rand_wall[1]] != '.'):
+                        maze[rand_wall[0] - 1][rand_wall[1]] = '#'
+                    if ([rand_wall[0] - 1, rand_wall[1]] not in walls):
+                        walls.append([rand_wall[0] - 1, rand_wall[1]])
+
+                if (rand_wall[1] != 0):
+                    if (maze[rand_wall[0]][rand_wall[1] - 1] != '.'):
+                        maze[rand_wall[0]][rand_wall[1] - 1] = '#'
+                    if ([rand_wall[0], rand_wall[1] - 1] not in walls):
+                        walls.append([rand_wall[0], rand_wall[1] - 1])
+
+                if (rand_wall[1] != width - 1):
+                    if (maze[rand_wall[0]][rand_wall[1] + 1] != '.'):
+                        maze[rand_wall[0]][rand_wall[1] + 1] = '#'
+                    if ([rand_wall[0], rand_wall[1] + 1] not in walls):
+                        walls.append([rand_wall[0], rand_wall[1] + 1])
+
+            for wall in walls:
+                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                    walls.remove(wall)
+
+            continue
+
+    if (rand_wall[0] != height - 1):
+        if (maze[rand_wall[0] + 1][rand_wall[1]] == '2' and maze[rand_wall[0] - 1][rand_wall[1]] == '.'):
+
+            s_cells = surroundingCells(rand_wall)
+            if (s_cells < 2):
+                maze[rand_wall[0]][rand_wall[1]] = '.'
+
+                if (rand_wall[0] != height - 1):
+                    if (maze[rand_wall[0] + 1][rand_wall[1]] != '.'):
+                        maze[rand_wall[0] + 1][rand_wall[1]] = '#'
+                    if ([rand_wall[0] + 1, rand_wall[1]] not in walls):
+                        walls.append([rand_wall[0] + 1, rand_wall[1]])
+                if (rand_wall[1] != 0):
+                    if (maze[rand_wall[0]][rand_wall[1] - 1] != '.'):
+                        maze[rand_wall[0]][rand_wall[1] - 1] = '#'
+                    if ([rand_wall[0], rand_wall[1] - 1] not in walls):
+                        walls.append([rand_wall[0], rand_wall[1] - 1])
+                if (rand_wall[1] != width - 1):
+                    if (maze[rand_wall[0]][rand_wall[1] + 1] != '.'):
+                        maze[rand_wall[0]][rand_wall[1] + 1] = '#'
+                    if ([rand_wall[0], rand_wall[1] + 1] not in walls):
+                        walls.append([rand_wall[0], rand_wall[1] + 1])
+
+            for wall in walls:
+                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                    walls.remove(wall)
+
+            continue
+
+    if (rand_wall[1] != width - 1):
+        if (maze[rand_wall[0]][rand_wall[1] + 1] == '2' and maze[rand_wall[0]][rand_wall[1] - 1] == '.'):
+
+            s_cells = surroundingCells(rand_wall)
+            if (s_cells < 2):
+                maze[rand_wall[0]][rand_wall[1]] = '.'
+
+                if (rand_wall[1] != width - 1):
+                    if (maze[rand_wall[0]][rand_wall[1] + 1] != '.'):
+                        maze[rand_wall[0]][rand_wall[1] + 1] = '#'
+                    if ([rand_wall[0], rand_wall[1] + 1] not in walls):
+                        walls.append([rand_wall[0], rand_wall[1] + 1])
+                if (rand_wall[0] != height - 1):
+                    if (maze[rand_wall[0] + 1][rand_wall[1]] != '.'):
+                        maze[rand_wall[0] + 1][rand_wall[1]] = '#'
+                    if ([rand_wall[0] + 1, rand_wall[1]] not in walls):
+                        walls.append([rand_wall[0] + 1, rand_wall[1]])
+                if (rand_wall[0] != 0):
+                    if (maze[rand_wall[0] - 1][rand_wall[1]] != '.'):
+                        maze[rand_wall[0] - 1][rand_wall[1]] = '#'
+                    if ([rand_wall[0] - 1, rand_wall[1]] not in walls):
+                        walls.append([rand_wall[0] - 1, rand_wall[1]])
+
+            for wall in walls:
+                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+                    walls.remove(wall)
+
+            continue
+
+    for wall in walls:
+        if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
+            walls.remove(wall)
+
+for i in range(0, height):
+    for j in range(0, width):
+        if (maze[i][j] == '2'):
+            maze[i][j] = '#'
+
+for i in range(0, width):
+    if (maze[1][i] == '#'):
+        maze[0][i] = '.'
+        break
+
+for i in range(width - 1, 0, -1):
+    if (maze[height - 2][i] == '.'):
+        maze[height - 1][i] = '.'
+        break
+
+printMaze(maze)
