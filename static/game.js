a7c918d453af9c144488e2306416bb7a2ed926a9
doodles09 <doodles230905@gmail.com>
2021-08-10 13:39:27 +0300

    start frontend

--- static/game.js
+++ static/game.js
@@ -1,21 +1,83 @@
+var ctx = canvas.getContext('2d')
+data = undefined
+var images = {}
+images.stone = new Image();
+images.stone.src = "/static/res/stone.jpg";
+
+images.grass = new Image();
+images.grass.src = "/static/res/grass_new.jpg";
+
+images.base = new Image();
+images.base.src = "/static/res/base.jpg";
+
+images.player = new Image();
+images.player.src = "/static/res/player.jpg";
+
 function drawGrass(){    
-    var ctx = canvas.getContext('2d')      
-    var testImage = new Image();
-    testImage.src = "CAPTURE-THE-FLAG/static/res/grass.jpg";
-    testImage.onload = function (){
-        ctx.drawImage(testImage, 10, 50)
+    for (var i = 0; i < 32; i++){
+        for (var j = 0; j < 32; j++){
+            ctx.drawImage(images.grass, 30 * i, 30 * j, 30, 30)
+        }
     }
 }
 
+simages.grass.onload = drawGrass
 
-function return_json(){
-    fetch('http://192.168.239.71:3333/api/state',
+function game_loop_iteration()
+{
+    if (!data) return
+    data['objects'].forEach((object) => {
+        if (object['type'] == 'wall') {
+            drawStone(object['x'], object['y'])
+        }
+    })
+    data['bases'].forEach((base) => {
+        if (base['type'] == 'bases') {
+            drawBase(base['x'], base['y'])
+        }   
+    })
+    data['players'].forEach((player) => {
+        drawPlayer(player['x'], player['y'])
+    })
+}
+
+
+// функции по отрисовке
+function drawStone(x, y){
+    return ctx.drawImage(images.stone, x * 30, y * 30, 30, 30)
+}
+
+function drawBase(x, y){
+    return ctx.drawImage(images.base, x * 30, y * 30, 30, 30)
+}
+
+function drawPlayer(x, y){
+    return ctx.drawImage(images.player, x, y, 30, 30)
+}
+
+// function drawBullet()
+
+// function drawPlayer(){
+// var ctx = canvas.getContext('2d')
+// var image = new Image();
+// image.src = "/static/res/red_square.jpg";  
+// image.onload = function (){
+//     ctx.drawImage(image, 510, 300, 30, 30)
+//     }
+// }
+
+
+function update_map(){
+    fetch('/api/state',
         ).then(response => {
-            console.log(response)
             return response.json()
         }
-        ).then(data => {
-            console.log(data);
-                alert(data.height)
+        ).then(answer => {
+            data = answer
+            game_loop_iteration()
+            console.log(data)
+            
         });
 }
+
+setInterval(update_map, 1000)
