79c4b08101e0745e45bbdf5c78e6ba3a37d1e08b
dmitry <faaful04@gmail.com>
2021-08-11 11:18:34 +0300

    add flag logic

--- ctf_game.py
+++ ctf_game.py
@@ -18,9 +18,9 @@ def get_choice(player, state):
         return None
 
 
-def is_it_a_wall(objects, x, y):
+def is_it_an_object(objects, x, y, type):
     for obj in objects:
-        if (obj["y"] == y) and (obj["x"] == x) and (obj["type"] == "wall"):
+        if (obj["y"] == y) and (obj["x"] == x) and (obj["type"] == type):
             return True
     return False
 
@@ -45,21 +45,23 @@ if __name__ == "__main__":
 
             for player in active_players:
                 if choices[player.id] == "go_up":
-                    if not is_it_a_wall(objects, player.x, (player.y - 1)) and (player.y != 0):
+                    if not is_it_an_object(objects, player.x, (player.y - 1), "wall") and (player.y != 0):
                         player.y -= 1
                     player.side = "up"
                 if choices[player.id] == "go_down":
-                    if not is_it_a_wall(objects, player.x, (player.y + 1)) and (player.y < 32):
+                    if not is_it_an_object(objects, player.x, (player.y + 1), "wall") and (player.y < 32):
                         player.y += 1
                     player.side = "down"
                 if choices[player.id] == "go_left":
-                    if not is_it_a_wall(objects, (player.x - 1), player.y) and (player.x != 0):
+                    if not is_it_an_object(objects, (player.x - 1), player.y, "wall") and (player.x != 0):
                         player.x -= 1
                     player.side = "left"
                 if choices[player.id] == "go_right":
-                    if not is_it_a_wall(objects, (player.x + 1), player.y) and (player.x < 32):
+                    if not is_it_an_object(objects, (player.x + 1), player.y, "wall") and (player.x < 32):
                         player.x += 1
                     player.side = "right"
+                if is_it_an_object(objects, player.x, player.y, "flag"):
+                    player.has_flag = True
 
                 print("Player {} is on ({}, {})".format(player.id, player.x, player.y))
 
