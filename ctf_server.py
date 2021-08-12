1902439827c8f47b6a871e3747c2ff6f1d21eb88
dmitry <faaful04@gmail.com>
2021-08-12 11:44:49 +0300

    add bullet

--- ctf_server.py
+++ ctf_server.py
@@ -18,7 +18,6 @@ migrate = Migrate(app, db)
 
 
 class User(db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100))
     username = db.Column(db.String(50), nullable=False, unique=True)
