adcb71484a39867f11dac9cf94a62867db928470
dmitry <faaful04@gmail.com>
2021-08-12 11:39:21 +0300

    add bullet

--- ctf_server.py
+++ ctf_server.py
@@ -18,6 +18,26 @@ migrate = Migrate(app, db)
 
 
 class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username
+        }
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.username)
+
+
+'''class User(db.Model):
     # ...
     updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
 
@@ -28,7 +48,7 @@ class User(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)'''
 
 
 class Player(db.Model):
