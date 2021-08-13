acb54caf0ce6737394c8d1153eb5d80f3a2feda2
dmitry <faaful04@gmail.com>
2021-08-13 14:11:28 +0300

    add bullets

--- examples/example_bot.py
+++ examples/example_bot.py
@@ -2,4 +2,4 @@ import random
 
 
 def make_choice(players, state):
-    return random.choice(["fire_up", "fire_down", "fire_right", "fire_left"])
+    return random.choice(["go_up", "go_down", "go_right", "go_left", "fire_up", "fire_down", "fire_right", "fire_left"])
